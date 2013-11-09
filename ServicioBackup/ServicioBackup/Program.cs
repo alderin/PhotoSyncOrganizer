0530fd7750768d421199bf7d70a25ab6b2f44cc3
Alejandro Del Rincón <alexsp92@gmail.com>
2013-11-09 12:37:09 +0100

    Añadidos los ficheros del proyecto

---
+++ ServicioBackup/ServicioBackup/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ServicioBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lista = Directory.EnumerateFiles(@"C:\Pruebas");
+            System.Text.ASCIIEncoding encoding = new System.Text.ASCIIEncoding();
+            foreach (var archivo in lista)
+            {     
+                Console.WriteLine(File.GetCreationTime(archivo));
+            }
+            Console.Read();
+        }
+
+        public void CrearCarpeta(DateTime fecha) { }
+    }
+}
---
+++ ServicioBackup/ServicioBackup/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("ServicioBackup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServicioBackup")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("748bea4d-a4bc-4617-817a-a218060beef3")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
