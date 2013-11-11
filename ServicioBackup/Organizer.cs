c2dc0dcf6288fd4ed2bfe2403039ba9f2d178ab7
Alejandro Del Rincón <alexsp92@gmail.com>
2013-11-11 22:09:13 +0100

    Ahora tenemos el tercer argumento como el número de días

--- ServicioBackup/Organizer.cs
+++ ServicioBackup/Organizer.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace ServicioBackup
 {
@@ -13,7 +13,7 @@ namespace ServicioBackup
 
         public String CarpetaDestinoImagenes { get; set; }
 
-
+        public TimeSpan IntervaloImagenes { get; set; }
 
         private String CrearCarpeta(DateTime date)
         {
@@ -24,25 +24,28 @@ namespace ServicioBackup
             }
             return path;
         }
+
         public void Ejecutar()
         {
             var lista = Directory.EnumerateFiles(CarpetaOrigenImagenes, "*.*", SearchOption.AllDirectories);
             foreach (var archivo in lista)
             {
-                if (MasViejaDeUnMes(archivo))
+                if (DebeSerEliminada(archivo))
                 {
                     var destino = CalcularPathDestino(archivo);
                     CopiarFichero(archivo, destino);
-                    Console.WriteLine( destino);
+                    Console.WriteLine(destino);
                 }
             }
             //File.CreateText(CarpetaDestinoImagenes + DateTime.Now.ToString() + ".txt");
         }
+
         private string CalcularPathDestino(string file)
         {
             string fechaArchivo = File.GetLastWriteTime(file).ToString("yyyy-MM-dd HH.mm.ss");
             return CrearCarpeta(Directory.GetLastWriteTime(file)) + fechaArchivo + Path.GetExtension(file);
         }
+
         private void CopiarFichero(string source, string destinationPath)
         {
             string destinationPathRenamed = ComprobarExistenciaFichero(destinationPath);
@@ -72,11 +75,12 @@ namespace ServicioBackup
             }
             return file;
         }
-        private bool MasViejaDeUnMes(string file)
+
+        private bool DebeSerEliminada(string file)
         {
             DateTime fechaArchivo = File.GetLastWriteTime(file);
             TimeSpan diferencia = DateTime.Now - fechaArchivo;
-            return diferencia > new TimeSpan(31, 0, 0, 0, 0);
+            return diferencia > IntervaloImagenes;
         }
     }
 }
--- ServicioBackup/Program.cs
+++ ServicioBackup/Program.cs
@@ -1,26 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
+
 namespace ServicioBackup
 {
-    class Program
+    internal class Program
     {
         /// <summary>
         /// Aplicación que se encarga de mover las fotos más viejas de un mes de una carpeta a otra.
         /// </summary>
         /// <param name="args">Acepta como argumentos la carpeta origen y la destino</param>
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-         //   Organizer org = new Organizer { CarpetaOrigenImagenes = @"C:\Users\Alejandro\Pictures\Movil", CarpetaDestinoImagenes = @"C:\Prueba2" };
-            OrganizadorImagenes org = new OrganizadorImagenes { CarpetaOrigenImagenes = args[0], CarpetaDestinoImagenes = args[1] };
-      
-            org.Ejecutar();
+            int dias;
+            // Organizer org = new Organizer { CarpetaOrigenImagenes =
+            // @"C:\Users\Alejandro\Pictures\Movil", CarpetaDestinoImagenes = @"C:\Prueba2" };
+            if (args.Length == 3)
+            {
+                if (int.TryParse(args[2], out dias))
+                {
+                    OrganizadorImagenes org = new OrganizadorImagenes { CarpetaOrigenImagenes = args[0], CarpetaDestinoImagenes = args[1], IntervaloImagenes = new TimeSpan(dias, 0, 0, 0) };
+                    org.Ejecutar();
+                }
+            }
+            else if (args.Length == 2)
+            {
+                OrganizadorImagenes org = new OrganizadorImagenes { CarpetaOrigenImagenes = args[0], CarpetaDestinoImagenes = args[1], IntervaloImagenes = new TimeSpan(30, 0, 0, 0) };
+                org.Ejecutar();
+            }
             //org.MoverArchivos();
         }
-
-
     }
 }
